786afd586a56a5a9302aa7862224e12b0ebb0038
Daniel <923daniel@naver.com>
2024-03-27 18:13:21 +0900

    refactor: LoginManager 클래스에 인터페이스 implement

--- src/main/java/manager/LoginManager.java
+++ src/main/java/manager/LoginManager.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import request.FileInfo;
 import request.HttpRequest;
-import request.RequestParser;
 import response.ContentType;
 import response.HttpResponse;
 import db.Database;
@@ -13,29 +12,13 @@ import db.Database;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
-public class LoginManager {
+public class LoginManager implements RequestManager {
     private static final Logger logger = LoggerFactory.getLogger(LoginManager.class);
 
-    HttpRequest httpRequest;
-    HttpResponse httpResponse;
-
-    public LoginManager(HttpRequest httpRequest){
-        this.httpRequest = httpRequest;
-        httpResponse = new HttpResponse();
-    }
-
-    public HttpResponse responseMaker() throws IOException {
-        if(httpRequest.isMethodGet()){
-            getResponseSetter();
-        }
-        if(httpRequest.isMethodPost()){
-            postResponseSetter();
-        }
-        return httpResponse;
-    }
-
-    private void getResponseSetter() throws IOException {  // 로그인 페이지로 이동하는 response 반환
+    @Override
+    public void getResponseSetter(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException { // 로그인 페이지로 이동하는 response 반환
         String completePath = FileInfo.makeCompletePath(httpRequest.getStartLineInfo("url"));
         String contextType = ContentType.getContentType(FileInfo.getFileType(completePath));
 
@@ -51,30 +34,35 @@ public class LoginManager {
         httpResponse.setBody(body);
     }
 
-    private void postResponseSetter() throws IOException { // 로그인 결과에 따라 response 반환
-        if(isLoginSuccess()){
-            loginSuccessResponse();
+    @Override
+    public void postResponseSetter(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        if(isLoginSuccess(httpRequest.getBodyInfo("userId"), httpRequest.getBodyInfo("password"))){
+            loginSuccessResponse(httpRequest, httpResponse);
         }else{
-            loginFailResponse();
+            loginFailResponse(httpResponse);
         }
     }
 
-    private void loginSuccessResponse() throws IOException { // 로그인 성공했을 때 response 만들기
+    private void loginSuccessResponse(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException { // 로그인 성공했을 때 response 만들기
         String completePath = FileInfo.makeCompletePath("/main");
         File file = new File(completePath);
         FileInputStream fis = new FileInputStream(file);
         byte[] body = fis.readAllBytes();
         fis.close();
 
-        httpResponse.setStartLine("302", "FOUND");
-        httpResponse.setLocation("/main/index.html");
+        body = showUserName(body, httpRequest); // name 포함한 html로 수정
+
+        httpResponse.setStartLine("200", "OK");
+        httpResponse.setContentType(ContentType.getContentType(FileInfo.getFileType(completePath)));
+        httpResponse.setContentLength(String.valueOf(body.length));
+
         String sessionId = Session.storeSession(httpRequest.getBodyInfo("userId")); // session db에 저장
         httpResponse.setCookie(sessionId);
 
         httpResponse.setBody(body);
     }
 
-    private void loginFailResponse() throws IOException { // 로그인 실패했을 때 response 만들기
+    private void loginFailResponse(HttpResponse httpResponse) throws IOException { // 로그인 실패했을 때 response 만들기
         String completePath = FileInfo.makeCompletePath("/login/fail");
         File file = new File(completePath);
         FileInputStream fis = new FileInputStream(file);
@@ -87,10 +75,7 @@ public class LoginManager {
         httpResponse.setBody(body);
     }
 
-    private boolean isLoginSuccess(){ // 로그인 성공 또는 실패 확인
-        String inputUserId = httpRequest.getBodyInfo("userId");
-        String inputPassword = httpRequest.getBodyInfo("password");
-
+    private boolean isLoginSuccess(String inputUserId, String inputPassword){ // 로그인 성공 또는 실패 확인
         if(Database.findUserById(inputUserId) == null){ // 해당 userId의 데이터가 없으면 false
             logger.error("아이디가 존재하지 않습니다! (input ID: {})", inputUserId);
             return false;
@@ -104,4 +89,10 @@ public class LoginManager {
         logger.info("로그인 성공! (login ID: {})", inputUserId);
         return true;
     }
+
+    private byte[] showUserName(byte[] body, HttpRequest httpRequest){ // 로그인 되어있을 때 user name 표시
+        String name = Database.findUserById(httpRequest.getBodyInfo("userId")).getName();
+        return new String(body, StandardCharsets.UTF_8).replace("<!--사용자 이름-->", name).getBytes(); // 사용자 name으로 대체하기
+    }
+
 }
