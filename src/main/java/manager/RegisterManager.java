d94ebc4924fee81f4ee0b60f26b44be4e92d053e
Daniel <923daniel@naver.com>
2024-03-27 18:09:34 +0900

    refactor: RegisterManager 클래스에 인터페이스 implement

--- src/main/java/manager/RegisterManager.java
+++ src/main/java/manager/RegisterManager.java
@@ -13,28 +13,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class RegisterManager {
+public class RegisterManager implements RequestManager {
     private static final Logger logger = LoggerFactory.getLogger(RegisterManager.class);
 
-    HttpRequest httpRequest;
-    HttpResponse httpResponse;
-
-    public RegisterManager(HttpRequest httpRequest){
-        this.httpRequest = httpRequest;
-        httpResponse = new HttpResponse();
-    }
-
-    public HttpResponse responseMaker() throws IOException {
-        if(httpRequest.isMethodGet()){
-            getResponseSetter();
-        }
-        if(httpRequest.isMethodPost()){
-            postResponseSetter();
-        }
-        return httpResponse;
-    }
-
-    private void getResponseSetter() throws IOException { // 회원가입 페이지로 이동하는 response 반환
+    @Override
+    public void getResponseSetter(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException { // 회원가입 페이지로 이동하는 response 반환
         String completePath = FileInfo.makeCompletePath(httpRequest.getStartLineInfo("url"));
         String contextType = ContentType.getContentType(FileInfo.getFileType(completePath));
 
@@ -50,8 +33,9 @@ public class RegisterManager {
         httpResponse.setBody(body);
     }
 
-    private void postResponseSetter() throws IOException { // 회원가입 후 main 페이지로 redirect 하는 response 반환
-        storeDatabase(createUser()); // request 정보로 User 객체 생성 후 db에 저장
+    @Override
+    public void postResponseSetter(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException { // 회원가입 후 main 페이지로 redirect 하는 response 반환
+        storeDatabase(createUser(httpRequest)); // request 정보로 User 객체 생성 후 db에 저장
         logger.info("회원가입 성공!");
 
         String completePath = FileInfo.makeCompletePath("/index.html");
@@ -67,7 +51,7 @@ public class RegisterManager {
         httpResponse.setBody(body);
     }
 
-    private User createUser(){ // httpRequest 정보를 통해 User객체 생성
+    private User createUser(HttpRequest httpRequest){ // httpRequest 정보를 통해 User객체 생성
         return new User(
                 httpRequest.getBodyInfo("userId"),
                 httpRequest.getBodyInfo("password"),
@@ -75,7 +59,6 @@ public class RegisterManager {
                 httpRequest.getBodyInfo("email"));
     }
 
-    // 회원가입 데이터 DB에 추가
     private void storeDatabase(User user){ // User 객체를 db에 저장
         Database.addUser(user);
     }
