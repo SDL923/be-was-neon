04239f7b2d18e0d9480b89d5a732dd48bdf5549c
Daniel <923daniel@naver.com>
2024-03-29 10:24:46 +0900

    feat: RegisterManagerTest 클래스 구현

---
+++ src/test/java/manager/RegisterManagerTest.java
@@ -0,0 +1,38 @@
+package manager;
+
+import db.Database;
+import model.User;
+import org.junit.jupiter.api.*;
+import request.HttpRequest;
+import response.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RegisterManagerTest {
+    private HttpRequest httpRequest;
+    private HttpResponse httpResponse;
+
+    @BeforeEach
+    void setRequestResponse() {
+        httpRequest = new HttpRequest();
+        httpResponse = new HttpResponse();
+    }
+
+    @Test
+    @DisplayName("static 폴더에 있는 파일의 경로를 반환해야 한다.")
+    void manageGetTest() {
+        httpRequest.storeStartLineData("POST /user/create HTTP/1.1");
+        httpRequest.storeHeadersData("Content-Length: 59"); // headers 중에 필요한 정보만 전달
+        httpRequest.storeBodyData("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
+
+        RegisterManager registerManager = new RegisterManager();
+        registerManager.storeDatabase(registerManager.createUser(httpRequest)); // User 객체 생성 후 DB에 저장
+
+        User user = Database.findUserById("javajigi");
+        assertThat(user.getUserId()).isEqualTo("javajigi");
+        assertThat(user.getPassword()).isEqualTo("password");
+        assertThat(user.getName()).isEqualTo("박재성");
+        assertThat(user.getEmail()).isEqualTo("javajigi@slipp.net");
+    }
+
+}
