fa14d76dc4e43a5c425160c480c10c682fa0d75f
Daniel <923daniel@naver.com>
2024-03-29 11:08:49 +0900

    주석 수정

--- src/test/java/request/HttpRequestTest.java
+++ src/test/java/request/HttpRequestTest.java
@@ -1,7 +1,5 @@
 package request;
 
-import db.Database;
-import model.User;
 import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,32 +33,17 @@ public class HttpRequestTest {
         assertThat(httpRequest.getStartLineInfo("version")).isEqualTo("HTTP/1.1");
     }
 
-//    @Test
-//    @DisplayName("POST 방식으로 전달된 회원가입 정보를 파싱할 수 있어야 한다.")
-//    void parsePostBodyData() {
-//        httpRequest.storeStartLineData("POST /user/create HTTP/1.1");
-//        httpRequest.storeHeadersData("Content-Length: 59"); // headers 중에 필요한 정보만 전달
-//        httpRequest.storeBodyData("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
-//
-//        assertThat(httpRequest.getStartLineInfo("userId")).isEqualTo("javajigi");
-//        assertThat(httpRequest.getStartLineInfo("password")).isEqualTo("password");
-//        assertThat(httpRequest.getStartLineInfo("name")).isEqualTo("박재성");
-//        assertThat(httpRequest.getStartLineInfo("email")).isEqualTo("javajigi@slipp.net");
-//    }
+    @Test
+    @DisplayName("POST 방식으로 전달된 회원가입 정보를 파싱할 수 있어야 한다.")
+    void parsePostBodyData() {
+        httpRequest.storeStartLineData("POST /user/create HTTP/1.1");
+        httpRequest.storeHeadersData("Content-Length: 59"); // headers 중에 필요한 정보만 전달
+        httpRequest.storeBodyData("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
 
-//    @Test
-//    @DisplayName("파싱한 회원가입 정보를 user 객체에 담은 후 DB에 저장해야 한다.")
-//    void storeUserAtDb() {
-//        httpRequest.storeStartLineData("POST /user/create HTTP/1.1");
-//        httpRequest.storeHeadersData("Content-Length: 59"); // headers 중에 필요한 정보만 전달
-//        httpRequest.storeBodyData("userId=javajigi&password=password&name=%EB%B0%95%EC%9E%AC%EC%84%B1&email=javajigi%40slipp.net");
-//        httpRequest.storeDatabase(httpRequest.createUser()); // User 객체 생성 후 DB에 저장
-//
-//        User user = Database.findUserById("javajigi");
-//        assertThat(user.getUserId()).isEqualTo("javajigi");
-//        assertThat(user.getPassword()).isEqualTo("password");
-//        assertThat(user.getName()).isEqualTo("박재성");
-//        assertThat(user.getEmail()).isEqualTo("javajigi@slipp.net");
-//    }
+        assertThat(httpRequest.getBodyInfo("userId")).isEqualTo("javajigi");
+        assertThat(httpRequest.getBodyInfo("password")).isEqualTo("password");
+        assertThat(httpRequest.getBodyInfo("name")).isEqualTo("박재성");
+        assertThat(httpRequest.getBodyInfo("email")).isEqualTo("javajigi@slipp.net");
+    }
 
 }
